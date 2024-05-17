732d420fa071708d24ace8621dc0cd675cc2981d
Stephan Meijer <stephan.meijer@gmail.com>
2024-05-17 14:54:38 +0200

    chore: also quote url in curl statement

--- src/to-curl.ts
+++ src/to-curl.ts
@@ -9,7 +9,7 @@ export async function toCurl(input: RequestInfo | URL, init?: RequestInit) {
 	const data = await request.text();
 
 	return [
-		`curl --url ${request.url}`,
+		`curl --url '${request.url}'`,
 		`--request ${request.method.toUpperCase()}`,
 		...headers.map(([key, value]) => `--header '${key}: ${value}'`),
 		data && `--data '${data}'`,
